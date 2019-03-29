28d3da3019085d2c91d0ac6dd15474688bf42b84
duongld2503 <ledinhduong2503@gmail.com>
2019-03-29 11:54:27 +0700

    game

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
-let count = 0;
-
+let count1 = 0;
+let count2 = 0;
 function change1(){
-    switch (count) {
+    switch (count1) {
         case 0:
-            cat1.src="dog1.jpg";
-            count++;
+            cat1.src = "dog1.jpg";
+            count1++;
             break;
         case 1:
-            cat1.src="chicken1.jpg";
-            count++;
+            cat1.src = "chicken1.jpg";
+            count1++;
             break;
         case 2:
-            cat1.src="turtle1.jpg";
-            count++;
+            cat1.src = "turtle1.jpg";
+            count1++;
             break;
         case 3:
-            cat1.src="cat1.jpg";
-            count = 0;
+            cat1.src = "cat1.jpg";
+            count1 = 0;
             break;
     }
 }
 
 function change2(){
-    switch (count) {
+    switch (count2) {
         case 0:
             cat2.src="dog2.jpg";
-            count++;
+            count2++;
             break;
         case 1:
             cat2.src="chicken2.jpg";
-            count++;
+            count2++;
             break;
         case 2:
             cat2.src="turtle2.jpg";
-            count++;
+            count2++;
             break;
         case 3:
             cat2.src="cat2.jpg";
-            count = 0;
+            count2 = 0;
             break;
     }
 }
+
+
 
 
